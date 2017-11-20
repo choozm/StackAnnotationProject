d5a6025e618f07d0a1344ac1b8867228f763b6ff
Choo Zhi Min <choozhimin@gmail.com>
2017-11-20 14:36:03 +0800

    Initial Java code and gradle files commit

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,16 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for default package");
+        System.out.println("Test for default package");
+        // $JUnit-BEGIN$
+        suite.addTestSuite(MyStringStackTest.class);
+        // $JUnit-END$
+        return suite;
+    }
+
+}
